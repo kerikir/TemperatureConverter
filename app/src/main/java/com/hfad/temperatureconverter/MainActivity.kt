a5e83cfed98c73663443acd6fda62bb6b311944d
kerikir <vlasov-kerik@mail.ru>
2025-12-07 08:54:00 +0300

    added the comments

--- app/src/main/java/com/hfad/temperatureconverter/MainActivity.kt
+++ app/src/main/java/com/hfad/temperatureconverter/MainActivity.kt
@@ -5,11 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 
 
+// Активность расширяет Component Activity
 class MainActivity : ComponentActivity() {
 
+    // Переопределяет onCreate()
     override fun onCreate(savedInstanceState: Bundle?) {
-
+        // Вызывает onCreate() в суперклассе
         super.onCreate(savedInstanceState)
+
+        // Задает содержимое активности
         setContent {
 
         }
