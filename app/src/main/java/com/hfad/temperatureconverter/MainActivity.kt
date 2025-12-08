f1e77c21feb25f85518338bcd6eec332ab3f7929
kerikir <vlasov-kerik@mail.ru>
2025-12-08 09:51:51 +0300

    added the extra settings in the image component

--- app/src/main/java/com/hfad/temperatureconverter/MainActivity.kt
+++ app/src/main/java/com/hfad/temperatureconverter/MainActivity.kt
@@ -4,9 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 
 // Активность расширяет Component Activity
@@ -36,9 +41,15 @@ fun MainActivityContent() {
     // Вывод избражения
     // painter - указывает какое изображение
     // content description - описание изображения для огр. возм.
+    // modifier - дополнительные настройки компонента Compose
+    // content scale - масштабирование изображения
     Image(
         painter = painterResource(R.drawable.sunrise),
-        contentDescription = "sunrise image"
+        contentDescription = "sunrise image",
+        modifier = Modifier
+            .height(180.dp)
+            .fillMaxWidth(),
+        contentScale = ContentScale.Crop
     )
 }
 
